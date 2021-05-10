f06defd60852e3463e37a0e407ac7bc4896f7102
hussainsr <hussain.rasiwala@mail.mcgill.ca>
2021-05-10 18:09:36 -0400

    Finished making the changes

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ After a successful run, please submit a pull request!
 def read_input():
     # Please allow the user to enter their first and last name from the terminal
     # Hint: We call this a raw input
-    first_name = ;
-    last_name = ;
+	print('\nPlease enter firstname:\t')
+	first_name = input()
+	print('\nPlease enter lasttname:\t')
+	last_name = input()
 
-    # Please fix the following print statement
-    'Welcome %s %s'%(first_name,last_name)
+	# Please fix the following print statement
+	print('\n\nWelcome %s %s'%(first_name,last_name))
 
-    #Supply the approriate return (We aren't actually returning anything :) )
-    return
 
 read_input()    
